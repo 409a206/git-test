3255d3a81d56b79c08642c7292f0298e4a3dde5e
user2 <user2@DESKTOP-3UTV0OG>
2020-03-23 17:18:22 +0900

    hello world 수정
    hello world 내용 삭제 및 hello world3
    추가

--- src/com/kh/gittest/Helloworld.java
+++ src/com/kh/gittest/Helloworld.java
@@ -3,8 +3,8 @@ package com.kh.gittest;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 	}
 
 }
